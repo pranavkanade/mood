eb9ab9b9f8e370e87c9034ab22a1f6fbe5eefb8d
pskanade <k.pranav.apk@gmail.com>
2018-08-05 23:31:36 +0530

    brought in db config

--- app.js
+++ app.js
@@ -3,21 +3,23 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+// DB config
+require("./config/db");
 const app = express();
 
-const poll = require('./routes/poll');
+const poll = require("./routes/poll");
 
 // Set public folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Body parser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Enable CORS
 app.use(cors());
 
-app.use('/poll', poll);
+app.use("/poll", poll);
 
 const port = 3000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
